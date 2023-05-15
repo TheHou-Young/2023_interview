c1843e9cdf429790559af829bc78123560164df4
TheHou-Young <1154387553@qq.com>
2023-05-16 02:28:31 +0800

    feat(time) add fn get startTime and endTime

--- utils/time/index.js
+++ utils/time/index.js
@@ -4,8 +4,9 @@ const moment = require("moment");
 // const seven_days_ms = 7 * one_day_ms;
 // 判断预约的时间是否有效——至少提前一天，至多提前七天
 const isValid = (current_time, reserve_start) => {
-  const start = moment(current_time).add(1, "days");
-  const end = moment(current_time).add(7, "days");
+  const time = moment(current_time);
+  const start = time.add(1, "days");
+  const end = time.add(7, "days");
   switch (true) {
     case reserve_start < start:
       return "late";
@@ -16,7 +17,12 @@ const isValid = (current_time, reserve_start) => {
   }
 };
 
-// 获取时间戳所在那天0时的时间戳
-const getDate = (time) => {};
+// 获取时间戳所在那天起止时间的时间戳
+const getStartAndEndTime = (time) => {
+  const date = moment(time);
+  const startTime = date.startOf("day").valueOf();
+  const endTime = date.endOf("day").valueOf();
+  return { startTime, endTime };
+};
 
-module.exports = { isValid };
+module.exports = { isValid, getStartAndEndTime };
