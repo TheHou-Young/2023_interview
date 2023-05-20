09133fab37aae5baff2c846dd7145095331654b5
BK201-Drama <1243532812@qq.com>
2023-05-20 15:27:04 +0800

    fix(delete): use req.body

--- controller/exercise.js
+++ controller/exercise.js
@@ -1,64 +1,60 @@
-const exerciseService = require("../service/exercise");
+const exerciseService = require('../service/exercise')
 
 class ExerciseController {
   async createExercise(req) {
-    const {
-      exercise_type,
-      exercise_desc,
-      exercise_level = "simple",
-    } = req.body;
+    const { exercise_type, exercise_desc, exercise_level = 'simple' } = req.body
     return await exerciseService.createExercise({
       exercise_type,
       exercise_desc,
       exercise_level,
-    });
+    })
   }
 
   async deleteExercise(req) {
-    const _id = req.query;
-    return await exerciseService.updateDeleteStatus(_id);
+    const { _id } = req.body
+    return await exerciseService.updateDeleteStatus(_id)
   }
 
   async updateExercise(req) {
-    const { _id, exercise_type, exercise_desc, exercise_level } = req.body;
+    const { _id, exercise_type, exercise_desc, exercise_level } = req.body
     return await exerciseService.updateExercise({
       _id,
       exercise_type,
       exercise_desc,
       exercise_level,
-    });
+    })
   }
 
   getExerciseType() {
-    return exerciseService.getExerciseType();
+    return exerciseService.getExerciseType()
   }
 
   getExerciseLevel() {
-    return exerciseService.getExerciseLevel();
+    return exerciseService.getExerciseLevel()
   }
 
   async getExerciseById(req) {
-    const _id = req.query;
-    return await exerciseService.findExerciseById(_id);
+    const _id = req.query
+    return await exerciseService.findExerciseById(_id)
   }
 
   async getExerciseByType(req) {
-    const { exercise_type } = req.body;
-    return await exerciseService.findExerciseByType({ exercise_type });
+    const { exercise_type } = req.body
+    return await exerciseService.findExerciseByType({ exercise_type })
   }
 
   // _id为面试记录的id
   async generateExercises(req) {
-    const { _id, exercise_types } = req.body;
-    return await exerciseService.generateExercises({ _id, exercise_types });
+    const { _id, exercise_types } = req.body
+    return await exerciseService.generateExercises({ _id, exercise_types })
   }
 
   async getList(req) {
-    const { exercise_type, exercise_level } = req.body;
-    return await exerciseService.getList({ exercise_type, exercise_level });
+    const { exercise_type, exercise_level } = req.body
+    return await exerciseService.getList({ exercise_type, exercise_level })
   }
 }
 
-const exerciseController = new ExerciseController();
+const exerciseController = new ExerciseController()
 
-module.exports = exerciseController;
+module.exports = exerciseController
--- controller/interviewrecord.js
+++ controller/interviewrecord.js
@@ -1,37 +1,37 @@
-const interviewrecordService = require("../service/interviewrecord");
+const interviewrecordService = require('../service/interviewrecord')
 
 class InterviewrecordController {
   async deleteRecord(req) {
-    const _id = req.query;
-    return await interviewrecordService.updateDeleteStatus(_id);
+    const { _id } = req.body
+    return await interviewrecordService.updateDeleteStatus(_id)
   }
 
   async updateRecord(req) {
-    const { _id, interview_evaluation } = req.body;
+    const { _id, interview_evaluation } = req.body
     return await interviewrecordService.updateRecord({
       _id,
       interview_evaluation,
-    });
+    })
   }
 
   // interview_mas为专家account
   async getRecordList_mas(req) {
-    const { interview_mas } = req.query;
-    return await interviewrecordService.getRecordList_mas({ interview_mas });
+    const { interview_mas } = req.query
+    return await interviewrecordService.getRecordList_mas({ interview_mas })
   }
 
   // interview_stu为学生account
   async getRecordList_stu(req) {
-    const { interview_stu } = req.query;
-    return await interviewrecordService.getRecordList_stu({ interview_stu });
+    const { interview_stu } = req.query
+    return await interviewrecordService.getRecordList_stu({ interview_stu })
   }
 
   async getExercises(req) {
-    const _id = req.query;
-    return await interviewrecordService.getExercises(_id);
+    const _id = req.query
+    return await interviewrecordService.getExercises(_id)
   }
 }
 
-const interviewrecordController = new InterviewrecordController();
+const interviewrecordController = new InterviewrecordController()
 
-module.exports = interviewrecordController;
+module.exports = interviewrecordController
