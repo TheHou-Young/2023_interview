3fa3fc3cf60707a099a3161263b681b512e3292c
BK201-Drama <1243532812@qq.com>
2023-05-21 19:12:53 +0800

    fix(record): get record by id

--- controller/interviewrecord.js
+++ controller/interviewrecord.js
@@ -1,50 +1,50 @@
-const interviewrecordService = require("../service/interviewrecord");
+const interviewrecordService = require('../service/interviewrecord')
 
 class InterviewrecordController {
   async deleteRecord(req) {
-    const { _id } = req.body;
-    return await interviewrecordService.updateDeleteStatus(_id);
+    const { _id } = req.body
+    return await interviewrecordService.updateDeleteStatus(_id)
   }
 
   async updateRecord(req) {
-    const { _id, interview_evaluation } = req.body;
+    const { _id, interview_evaluation } = req.body
     return await interviewrecordService.updateRecord({
       _id,
       interview_evaluation,
-    });
+    })
   }
 
   async getRecordById(req) {
-    const { _id } = req.body;
-    return await interviewrecordService.getRecordById(_id);
+    const { _id } = req.query
+    return await interviewrecordService.getRecordById(_id)
   }
 
   // interview_mas为专家account
   async getRecordList_mas(req) {
-    const { interview_mas, page = 1, size = 10 } = req.query;
+    const { interview_mas, page = 1, size = 10 } = req.query
     return await interviewrecordService.getRecordList_mas({
       interview_mas,
       page,
       size,
-    });
+    })
   }
 
   // interview_stu为学生account
   async getRecordList_stu(req) {
-    const { interview_stu, page = 1, size = 10 } = req.query;
+    const { interview_stu, page = 1, size = 10 } = req.query
     return await interviewrecordService.getRecordList_stu({
       interview_stu,
       page,
       size,
-    });
+    })
   }
 
   async getExercises(req) {
-    const _id = req.query;
-    return await interviewrecordService.getExercises(_id);
+    const _id = req.query
+    return await interviewrecordService.getExercises(_id)
   }
 }
 
-const interviewrecordController = new InterviewrecordController();
+const interviewrecordController = new InterviewrecordController()
 
-module.exports = interviewrecordController;
+module.exports = interviewrecordController
